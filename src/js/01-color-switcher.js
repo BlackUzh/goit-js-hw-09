fd048baf874a9bb69da51bf82fb4b3afb621912d
Oleksandr Shmaida <black7panthera@gmail.com>
2023-04-17 14:55:47 +0300

    task 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector("[data-start]")
+const stopBtn = document.querySelector("[data-stop]")
+const body = document.querySelector("body")
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+
+const onChangeColor = () => {
+    startBtn.disabled = true
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+}
+
+const offChangeColor = () => {
+    clearInterval(intervalId)
+    startBtn.disabled = false
+}
+
+startBtn.addEventListener("click", onChangeColor);
+stopBtn.addEventListener("click", offChangeColor)
